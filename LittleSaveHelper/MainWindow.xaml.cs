dd83eea95c98dc23ec516a1d6a1f8f3c15238bb2
KonradNowakowski <41647669+KonradNowakowski@users.noreply.github.com>
2018-10-04 19:19:57 +0200

    Third Commit

--- LittleSaveHelper/MainWindow.xaml.cs
+++ LittleSaveHelper/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace LittleSaveHelper
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LittleSaveHelper
 {
     /// <summary>
     /// Logic of xaml file
@@ -8,6 +12,28 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            ItemList.Items.Add(new SaveInfo {GameName = "GTA 5", LastBackupTime = DateTime.Now});
+            string a = DateTime.Now.Year.ToString();
+        }
+
+        private void DeleteButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        
+        private void ItemList_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private class SaveInfo
+        {
+            public string GameName { get; set; }
+            public DateTime LastBackupTime { get; set; }
         }
     }
 }
